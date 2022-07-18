6273268956b8df841e3e55d3fc0d4764cb1c527f
Buglakova <elena.v.buglakova@gmail.com>
2022-07-18 14:33:14 +0200

    Plot projections of samples.

--- torch_em/util/debug.py
+++ torch_em/util/debug.py
@@ -1,7 +1,7 @@
 import warnings
 import torch
 from .util import ensure_array
-
+import numpy as np
 
 def _check_plt(loader, n_samples, instance_labels, model=None, device=None):
     import matplotlib.pyplot as plt
@@ -27,14 +27,32 @@ def _check_plt(loader, n_samples, instance_labels, model=None, device=None):
         # cast the data to array and remove the batch axis / choose first sample in batch
         x = ensure_array(x)[0]
         y = ensure_array(y)[0]
+
+        print(f"Label shape: {y.shape}")
+        print(f"Number of annotated pixels: {len(y[y != -1])} out of {y.size}")
+        print(f"Number of annotated slices: {len(y[y != -1])} out of {y.size}")
         assert x.ndim == y.ndim
+
         if x.ndim == 4:  # 3d data (with channel axis)
-            z_slice = x.shape[1] // 2
-            warnings.warn(f"3d input data is not yet supported, will only show slice {z_slice} / {x.shape[1]}")
-            x, y = x[:, z_slice], y[:, z_slice]
+            x_slice = np.s_[:, :, x.shape[3] // 2]
+            y_slice = np.s_[:, x.shape[2] // 2, :]
+            z_slice = np.s_[x.shape[1] // 2, :, :]
+
+            # warnings.warn(f"3d input data is not yet supported, will only show slice {z_slice} / {x.shape[1]}")
+            # x, y = x[:, z_slice], y[:, z_slice]
+            print(f"3D data, showing slices at {(x.shape[3] // 2, x.shape[2] // 2, x.shape[1] // 2)} out of {x.shape}")
             if pred is not None:
                 pred = pred[:, z_slice]
+            n_projections = 3
 
+        if x.ndim < 4:
+            x = x[:, np.newaxis, ...]
+            y = y[:, np.newaxis, ...]
+            if pred is not None:
+                pred = pred[:, np.newaxis, ...]
+
+            n_projections = 1
+            z_slice = np.s_
         if x.shape[0] > 1:
             warnings.warn(f"Multi-channel input data is not yet supported, will only show channel 0 / {x.shape[0]}")
         x = x[0]
@@ -48,24 +66,69 @@ def _check_plt(loader, n_samples, instance_labels, model=None, device=None):
 
         if fig is None:
             n_rows = n_target_channels + 1 if pred is None else 2 * n_target_channels + 1
-            fig = plt.figure(figsize=(n_samples*img_size, n_rows*img_size))
+            fig = plt.figure(figsize=((n_samples  * n_projections) * img_size, n_rows * img_size))
 
-        ax = fig.add_subplot(n_rows, n_samples, to_index(n_samples, 0, ii))
-        ax.imshow(x, interpolation="nearest", cmap="Greys_r", aspect="auto")
+        ax = fig.add_subplot(n_rows, n_samples * n_projections, to_index(n_samples * n_projections, 0, ii * n_projections))
+        ax.imshow(x[z_slice], interpolation="nearest", cmap="Greys_r", aspect="auto")
+        ax.set_xlabel("x")
+        ax.set_ylabel("y")
 
+        plots_kwargs = {"interpolation": "nearest", "cmap": "Greys_r", "aspect": "auto", "vmin": y.min(), "vmax": y.max() }
+
+        if n_projections == 3:
+            ax = fig.add_subplot(n_rows, n_samples * n_projections, to_index(n_samples * n_projections, 0, ii * n_projections + 1))
+            ax.imshow(x[y_slice], interpolation="nearest", cmap="Greys_r", aspect="auto")
+            ax.set_xlabel("x")
+            ax.set_ylabel("z")
+
+            ax = fig.add_subplot(n_rows, n_samples * n_projections, to_index(n_samples * n_projections, 0, ii * n_projections + 2))
+            ax.imshow(x[x_slice], interpolation="nearest", cmap="Greys_r", aspect="auto")
+            ax.set_xlabel("y")
+            ax.set_ylabel("z")
         for chan in range(n_target_channels):
-            ax = fig.add_subplot(n_rows, n_samples, to_index(n_samples, 1 + chan, ii))
+            ax = fig.add_subplot(n_rows, n_samples * n_projections, to_index(n_samples * n_projections, 1 + chan, ii * n_projections))
             if instance_labels:
-                ax.imshow(y[chan].astype("uint32"), interpolation="nearest", aspect="auto")
+                ax.imshow(y[chan][z_slice].astype("uint32"), interpolation="nearest", aspect="auto")
             else:
-                ax.imshow(y[chan], interpolation="nearest", cmap="Greys_r", aspect="auto")
+                ax.imshow(y[chan][z_slice], **plots_kwargs)
+            ax.set_xlabel("x")
+            ax.set_ylabel("y")
+
+            if n_projections == 3:
+                ax = fig.add_subplot(n_rows, n_samples * n_projections, to_index(n_samples * n_projections, 1 + chan, ii * n_projections + 1))
+                if instance_labels:
+                    ax.imshow(y[chan][y_slice].astype("uint32"), interpolation="nearest", aspect="auto")
+                else:
+                    ax.imshow(y[chan][y_slice], **plots_kwargs)
+                ax.set_xlabel("x")
+                ax.set_ylabel("z")
+
+                ax = fig.add_subplot(n_rows, n_samples * n_projections, to_index(n_samples * n_projections, 1 + chan, ii * n_projections + 2))
+                if instance_labels:
+                    ax.imshow(y[chan][x_slice].astype("uint32"), interpolation="nearest", aspect="auto")
+                else:
+                    ax.imshow(y[chan][x_slice], **plots_kwargs)
+                ax.set_xlabel("y")
+                ax.set_ylabel("z")
 
         if pred is not None:
             for chan in range(n_target_channels):
-                ax = fig.add_subplot(n_rows, n_samples, to_index(n_samples, 1 + n_target_channels + chan, ii))
-                ax.imshow(pred[chan], interpolation="nearest", cmap="Greys_r", aspect="auto")
+                ax = fig.add_subplot(n_rows, n_samples * n_projections, to_index(n_samples * n_projections, 1 + n_target_channels + chan, ii * n_projections))
+                ax.imshow(pred[chan][z_slice], **plots_kwargs)
+
+                if n_projections == 3:
+                    ax = fig.add_subplot(n_rows, n_samples * n_projections, to_index(n_samples * n_projections, 1 + chan, ii * n_projections + 1))
+                    ax.imshow(pred[chan][y_slice], **plots_kwargs)
+                    ax.set_xlabel("x")
+                    ax.set_ylabel("z")
+
+                    ax = fig.add_subplot(n_rows, n_samples * n_projections, to_index(n_samples * n_projections, 1 + chan, ii * n_projections + 2))
+                    ax.imshow(pred[chan][x_slice], **plots_kwargs)
+                    ax.set_xlabel("y")
+                    ax.set_ylabel("z")
 
     plt.show()
+    return fig
 
 
 def _check_napari(loader, n_samples, instance_labels, model=None, device=None):
